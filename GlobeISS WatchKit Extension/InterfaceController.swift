1019eec940d348aff43312bc834393a4f4525756
Dylan Maryk <dylan@door2door.io>
2020-05-02 23:39:44 +0200

    Add globe with ability to rotate and zoom

--- GlobeISS WatchKit Extension/InterfaceController.swift
+++ GlobeISS WatchKit Extension/InterfaceController.swift
@@ -3,29 +3,65 @@
 //  GlobeISS WatchKit Extension
 //
 //  Created by Dylan Maryk on 02/05/2020.
-//  Copyright © 2020 dylanmaryk. All rights reserved.
+//  Copyright © 2020 Dylan Maryk. All rights reserved.
 //
 
 import WatchKit
-import Foundation
 
-
-class InterfaceController: WKInterfaceController {
-
+class InterfaceController: WKInterfaceController, WKCrownDelegate {
+    
+    @IBOutlet private weak var sceneView: WKInterfaceSCNScene!
+    
+    private lazy var globeNode = self.createGlobeNode()
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
-        // Configure interface objects here.
+        let scene = SCNScene()
+        scene.rootNode.addChildNode(self.globeNode)
+        sceneView.scene = scene
+        
+        self.crownSequencer.delegate = self
+        self.crownSequencer.focus()
     }
     
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
+    @IBAction func panGesture(_ sender: Any) {
+        let gestureRecognizer = sender as! WKPanGestureRecognizer
+        let translation = gestureRecognizer.translationInObject()
+        let x = translation.x
+        let y = -translation.y
+        let anglePan = sqrt(pow(x, 2) + pow(y, 2)) * .pi / 180
+        let rotationVector = SCNVector4(-y, x, 0, anglePan)
+        self.globeNode.rotation = rotationVector
+        
+        if gestureRecognizer.state == .ended {
+            let currentPivot = self.globeNode.pivot
+            let currentPosition = self.globeNode.position
+            let changePivot = SCNMatrix4Invert(SCNMatrix4MakeRotation(self.globeNode.rotation.w,
+                                                                      self.globeNode.rotation.x,
+                                                                      self.globeNode.rotation.y,
+                                                                      self.globeNode.rotation.z))
+            self.globeNode.pivot = SCNMatrix4Mult(changePivot, currentPivot)
+            self.globeNode.transform = SCNMatrix4Identity
+            self.globeNode.position = currentPosition
+        }
     }
     
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
+    func crownDidRotate(_ crownSequencer: WKCrownSequencer?, rotationalDelta: Double) {
+        self.globeNode.runAction(.moveBy(x: 0, y: 0, z: CGFloat(rotationalDelta), duration: 0))
     }
-
+    
+    private func createGlobeNode() -> SCNNode {
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named: "snapshot-2020-05-1")
+        
+        let sphere = SCNSphere(radius: 1)
+        sphere.firstMaterial = material
+        
+        let sphereNode = SCNNode(geometry: sphere)
+        sphereNode.position = SCNVector3(0, 0, 0)
+        
+        return sphereNode
+    }
+    
 }
